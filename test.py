3e812944ca3a7b80fbfef193b1e19ac873b8f464
guillermoim <guiinfmol@yahoo.com>
2020-02-28 16:12:17 +0100

    Ready

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from crawler import WebCrawler
 import pandas
 
 if __name__ == '__main__':
-    wc = WebCrawler('https://www.youtube.com/watch?v=wfbEoq634ZY')
-    wc.searchBFS('YouTube')
+    wc = WebCrawler('https://www.reddit.com/', limit=10)
+    wc.searchBFS('reddit')
 
